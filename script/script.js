a384c9acc57ae4e253f5e3afe33e64a0a1501178
Willian-Louback <williandlouback@gmail.com>
2022-07-26 16:13:33 -0300

    Suporte para mais Pokémon

--- script/script.js
+++ script/script.js
@@ -1,7 +1,6 @@
 var form = document.querySelector('.input');
-var ativador = "";
 var envio = document.querySelector('.envio');
-const gif = document.querySelector('.gif-pokemom');
+const gif = document.querySelector('.gif-pokemon');
 var id = document.querySelector('.numero');
 id = document.querySelector('.numero').innerHTML = "1";
 document.querySelector('.nome').innerHTML = "Bulbasaur";
@@ -10,38 +9,46 @@ gif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/poke
 form.addEventListener('submit', async(event) => {
     event.preventDefault();
     document.querySelector('.nome').innerHTML = "Carregando...";
+    document.querySelector('.gif-pokemon').src = "../imagens/loading-gif.webp";
     document.querySelector(".numero").style.display = "none";
     document.querySelector(".traco").style.display = "none";
     const resposta = document.querySelector('.envio').value.toLowerCase();
     const link = await fetch("https://pokeapi.co/api/v2/pokemon/" + resposta);
     if(link.status != 200){
-        gif.src = "";
+        gif.src = "../imagens/Psyduck-confuso-error.png";
         document.querySelector(".nome").innerHTML = "Não encontrei :(";
         id = "";
     } else if (envio.value == ""){
-        gif.src = "";
+        gif.src = "../imagens/Psyduck-confuso-error.png";
         document.querySelector(".nome").innerHTML = "(Insira algum valor)";
         id = "";
-    }else{
+    } else {
         const data = await link.json();
-        id = data.id;
+        id = data.id; console.log(id);
         document.querySelector('.numero').innerHTML = id;
         document.querySelector('.numero').style.display = "inline-block";
         document.querySelector('.traco').style.display = "inline-block";
         const nome = document.querySelector(".nome");
         nome.innerHTML = data.name[0].toUpperCase() + data.name.slice(1);
-        gif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/" + id + ".gif";
-        ativador = "ok";
+        if (650 <= id && id <= 898){
+            gif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + id + ".png";
+        } else if(id > 898){
+            gif.src = "../imagens/Psyduck-confuso-error.png";
+            alert("Este Pokémon ainda não possui sprite...");
+        } else {
+            gif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/" + id + ".gif";
+        }
     }
     envio.value = "";
 });
 function shiny(){
-    if(id != ""){
+    if(id != "" && id >= 1 && id <= 649){
         gif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/" + id + ".gif";
+    } else if(id != "" && id >= 150 && id <= 898){
+        gif.src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/"+ id + ".png";
     } else if (id == ""){
         alert("Escolha algum Pokémon primeiro...")
     }
-    ativador = "";
 }
 // link shiny charizard https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/shiny/6.gif
 //const id = document.querySelector(".numero").innerHTML = data.id;
